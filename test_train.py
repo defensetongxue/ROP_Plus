f7e137f7cc697da974eb9add1639369a5380abd1
guoyi <1900013009@pku.edu.cn>
2023-02-15 21:31:58 +0800

    test

--- test_train.py
+++ test_train.py
@@ -21,6 +21,7 @@ print("begin_trainning process")
 for epoch in range(10):
 
 	#training------------------------
+	model.train()
 	train_loss = 0
 	train_accu = 0
 	step = 0
@@ -31,7 +32,6 @@ for epoch in range(10):
 
 		optimizer = optim.Adam(model.parameters(),lr=0.001)
 		loss = loss_func(out1,batch_y)
-		print(loss.data)
 		train_loss += loss.data.item()
 		pred = torch.max(out1,1)[1]
 		train_correct = (pred==batch_y).sum()
@@ -39,28 +39,29 @@ for epoch in range(10):
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
-		print("EPOCH:%d,STEP:%d"%(epoch,step))
+		# print("EPOCH:%d,STEP:%d"%(epoch,step))
 		step = step + 1
 	mean_loss = train_loss/(len(train_data))
 	mean_accu = train_accu/(len(train_data))
 	#print(mean_loss,mean_accu)
-	print('Training Loss : %.6f,Accu: %.6f'%(mean_loss,mean_accu))
+	print('epoch :{} Training Loss : {:.6f},Accu: {:.6f}'.format(epoch,mean_loss,mean_accu))
 											
 	#evaluation------------------------
 	model.eval()
-	eval_loss = 0
-	eval_accu = 0
-	for batch_x,batch_y in test_loader:
-		batch_x,batch_y = Variable(batch_x,volatile=True).cuda(),Variable(batch_y,volatile=True).cuda()
-		out = model(batch_x)
-		loss = loss_func(out,batch_y)
-		eval_loss += loss.data.item()
-		pred = torch.max(out,1)[1]
-		num_correct = (pred == batch_y).sum()
-		eval_accu += num_correct.data.item()
-	mean_loss = eval_loss/(len(test_data))
-	mean_accu = eval_accu/(len(test_data))
-	print('Testing Loss:%.6f,Accu:%.6f'%(mean_loss,mean_accu))
+	with torch.no_grad():
+		eval_loss = 0
+		eval_accu = 0
+		for batch_x,batch_y in test_loader:
+			batch_x,batch_y = Variable(batch_x).cuda(),Variable(batch_y).cuda()
+			out = model(batch_x)
+			loss = loss_func(out,batch_y)
+			eval_loss += loss.data.item()
+			pred = torch.max(out,1)[1]
+			num_correct = (pred == batch_y).sum()
+			eval_accu += num_correct.data.item()
+		mean_loss = eval_loss/(len(test_data))
+		mean_accu = eval_accu/(len(test_data))
+		print('Testing Loss:{:.6f},Accu:{:.6f}'.format(mean_loss,mean_accu))
 
 	name = 'inception3'+'_'+str(epoch)+'.pkl'
 	torch.save(model,'./models/'+name)
