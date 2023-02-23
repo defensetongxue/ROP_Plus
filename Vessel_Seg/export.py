0d2463e2881d1e3a15d48cfb321dce1bf8cbfdc0
guoyi <1900013009@pku.edu.cn>
2023-02-23 15:05:08 +0800

    fix vesSegModel setting

--- Vessel_Seg/export.py
+++ Vessel_Seg/export.py
@@ -2,18 +2,27 @@ import torch
 from models.LadderNet import LadderNet
 from tmp_lib import *
 from torchvision import datasets, transforms
-import numpy as np
+import numpy as npp
 from torch.utils.data import DataLoader
 import torch.nn as nn
 
 
 class vessel_seg_model(nn.Module):
-    def __init__(self, model_name="LadderNet",
+    def __init__(self,patch_height,
+                   patch_width,
+                   stride_height,
+                   stride_width ,
+                   model_name="LadderNet",
                  pretrained=True,
                  pretrain_path="./Vessel_Seg/save_model/best_model.pth"):
         super(vessel_seg_model, self).__init__()
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
+        self.patch_height=patch_height
+        self.patch_width=patch_width
+        self.stride_height=stride_height
+        self.stride_width=stride_width
+
         self.pretrained = pretrained
         self.pretrain_path = pretrain_path
 
@@ -30,16 +39,12 @@ class vessel_seg_model(nn.Module):
                     self.pretrain_path)
             return net
 
-    def forward(self, data,
-                test_patch_height,
-                test_patch_width, stride_height,
-                stride_width):
+    def forward(self, data):
         '''
         receive a type data set and return an new dataset
         '''
         img_height, img_width = data.shape[2], data.shape[3]
-        data_loader = self.preprocess(
-            data, test_patch_height, test_patch_width, stride_height, stride_width)
+        data_loader = self.preprocess(data)
         preds = []
         for patch in data_loader:
             outputs = self.model(patch.to(self.device))
@@ -49,104 +54,94 @@ class vessel_seg_model(nn.Module):
         pred_patches = np.expand_dims(predictions, axis=1)
 
         pred_imgs = self.recompone_overlap(
-            pred_patches, img_height, img_width, stride_height, stride_width)
+            pred_patches, img_height, img_width)
         pred_imgs = pred_imgs[:, :, 0:img_height, 0:img_width]
         return pred_imgs
 
-    def preprocess(self, img,
-                   test_patch_height,
-                   test_patch_width,
-                   stride_height,
-                   stride_width):
+    def preprocess(self, img):
         img = np.array(img)
         imgs = rgb2gray(img)
         imgs = dataset_normalized(imgs)
         imgs = clahe_equalized(imgs)
         imgs = adjust_gamma(imgs, 1.2)
         test_imgs = imgs/255
-        test_imgs = self.paint_border_overlap(
-            test_imgs, test_patch_height, test_patch_width, stride_height, stride_width)
-        patches_imgs_test = self.extract_ordered_overlap(
-            test_imgs, test_patch_height, test_patch_width, stride_height, stride_width)
+        test_imgs = self.paint_border_overlap(test_imgs)
+        patches_imgs_test = self.extract_ordered_overlap(test_imgs)
         test_set = TestDataset(patches_imgs_test)
         test_loader = DataLoader(
             test_set, batch_size=64, shuffle=False, num_workers=3)
         return test_loader
 
-    def paint_border_overlap(full_imgs,
-                             patch_h, patch_w,
-                             stride_h, stride_w):
+    def paint_border_overlap(self,full_imgs):
         assert (len(full_imgs.shape) == 4)  # 4D arrays
         # check the channel is 1 or 3
         assert (full_imgs.shape[1] == 1 or full_imgs.shape[1] == 3)
         img_h = full_imgs.shape[2]  # height of the image
         img_w = full_imgs.shape[3]  # width of the image
-        leftover_h = (img_h-patch_h) % stride_h  # leftover on the h dim
-        leftover_w = (img_w-patch_w) % stride_w  # leftover on the w dim
+        leftover_h = (img_h-self.patch_height) % self.stride_height  # leftover on the h dim
+        leftover_w = (img_w-self.patch_width) % self.stride_width  # leftover on the w dim
         if (leftover_h != 0):  # change dimension of img_h
             print(
-                "\nthe side H is not compatible with the selected stride of " + str(stride_h))
-            # print("img_h " +str(img_h) + ", patch_h " +str(patch_h) + ", stride_h " +str(stride_h))
+                "\nthe side H is not compatible with the selected stride of " + str(self.stride_height))
             print("(img_h - patch_h) MOD stride_h: " + str(leftover_h))
             print("So the H dim will be padded with additional " +
-                  str(stride_h - leftover_h) + " pixels")
+                  str(self.stride_height - leftover_h) + " pixels")
             tmp_full_imgs = np.zeros(
-                (full_imgs.shape[0], full_imgs.shape[1], img_h+(stride_h-leftover_h), img_w))
+                (full_imgs.shape[0], full_imgs.shape[1], img_h+(self.stride_height-leftover_h), img_w))
             tmp_full_imgs[0:full_imgs.shape[0],
                           0:full_imgs.shape[1], 0:img_h, 0:img_w] = full_imgs
             full_imgs = tmp_full_imgs
         if (leftover_w != 0):  # change dimension of img_w
             print(
-                "the side W is not compatible with the selected stride of " + str(stride_w))
-            # print("img_w " +str(img_w) + ", patch_w " +str(patch_w) + ", stride_w " +str(stride_w))
+                "the side W is not compatible with the selected stride of " + str(self.stride_width))
             print("(img_w - patch_w) MOD stride_w: " + str(leftover_w))
             print("So the W dim will be padded with additional " +
-                  str(stride_w - leftover_w) + " pixels")
+                  str(self.stride_width - leftover_w) + " pixels")
             tmp_full_imgs = np.zeros(
-                (full_imgs.shape[0], full_imgs.shape[1], full_imgs.shape[2], img_w+(stride_w - leftover_w)))
+                (full_imgs.shape[0], full_imgs.shape[1], full_imgs.shape[2], img_w+(self.stride_width - leftover_w)))
             tmp_full_imgs[0:full_imgs.shape[0], 0:full_imgs.shape[1],
                           0:full_imgs.shape[2], 0:img_w] = full_imgs
             full_imgs = tmp_full_imgs
         print("new padded images shape: " + str(full_imgs.shape))
         return full_imgs
 
-    def extract_ordered_overlap(full_imgs, patch_h, patch_w, stride_h, stride_w):
+    def extract_ordered_overlap(self,full_imgs):
         assert (len(full_imgs.shape) == 4)  # 4D arrays
         # check the channel is 1 or 3
         assert (full_imgs.shape[1] == 1 or full_imgs.shape[1] == 3)
         img_h = full_imgs.shape[2]  # height of the full image
         img_w = full_imgs.shape[3]  # width of the full image
-        assert ((img_h-patch_h) % stride_h ==
-                0 and (img_w-patch_w) % stride_w == 0)
+        assert ((img_h-self.patch_height) % self.stride_height ==
+                0 and (img_w-self.patch_width) % self.stride_width == 0)
         # // --> division between integers
-        N_patches_img = ((img_h-patch_h)//stride_h+1) * \
-            ((img_w-patch_w)//stride_w+1)
+        N_patches_img = ((img_h-self.patch_height)//self.stride_height+1) * \
+            ((img_w-self.patch_width)//self.stride_width+1)
         N_patches_tot = N_patches_img*full_imgs.shape[0]
-        print("Number of patches on h : " + str(((img_h-patch_h)//stride_h+1)))
-        print("Number of patches on w : " + str(((img_w-patch_w)//stride_w+1)))
+        print("Number of patches on h : " + str(((img_h-self.patch_height)//self.stride_height+1)))
+        print("Number of patches on w : " + str(((img_w-self.patch_width)//self.stride_width+1)))
         print("number of patches per image: " + str(N_patches_img) +
               ", totally for testset: " + str(N_patches_tot))
         patches = np.empty(
-            (N_patches_tot, full_imgs.shape[1], patch_h, patch_w))
+            (N_patches_tot, full_imgs.shape[1], self.patch_height, self.patch_width))
         iter_tot = 0  # iter over the total number of patches (N_patches)
         for i in range(full_imgs.shape[0]):  # loop over the full images
-            for h in range((img_h-patch_h)//stride_h+1):
-                for w in range((img_w-patch_w)//stride_w+1):
-                    patch = full_imgs[i, :, h*stride_h:(
-                        h*stride_h)+patch_h, w*stride_w:(w*stride_w)+patch_w]
+            for h in range((img_h-self.patch_height)//self.stride_height+1):
+                for w in range((img_w-self.patch_width)//self.stride_width+1):
+                    patch = full_imgs[i, :, h*self.stride_height:(
+                        h*self.stride_height)+self.patch_height, w*self.stride_width:(w*self.stride_width)+self.patch_width]
                     patches[iter_tot] = patch
                     iter_tot += 1  # total
         return patches  # array with all the full_imgs divided in patches
 
-    def recompone_overlap(preds, img_h, img_w, stride_h, stride_w):
+    def recompone_overlap(self,preds, img_h, img_w):
 
         assert (len(preds.shape) == 4)  # 4D arrays
         assert (preds.shape[1] == 1 or preds.shape[1]
                 == 3)  # check the channel is 1 or 3
         patch_h = preds.shape[2]
         patch_w = preds.shape[3]
-        N_patches_h = (img_h-patch_h)//stride_h+1
-        N_patches_w = (img_w-patch_w)//stride_w+1
+        N_patches_h = (img_h-patch_h)//self.stride_height+1
+        N_patches_w = (img_w-patch_w)//self.stride_width+1
         N_patches_img = N_patches_h * N_patches_w
         # print("N_patches_h: " + str(N_patches_h))
         # print("N_patches_w: " + str(N_patches_w))
@@ -159,14 +154,14 @@ class vessel_seg_model(nn.Module):
 
         k = 0  # iterator over all the patches
         for i in range(N_full_imgs):
-            for h in range((img_h-patch_h)//stride_h+1):
-                for w in range((img_w-patch_w)//stride_w+1):
+            for h in range((img_h-patch_h)//self.stride_height+1):
+                for w in range((img_w-patch_w)//self.stride_width+1):
                     # Accumulate predicted values
-                    full_prob[i, :, h*stride_h:(h*stride_h)+patch_h,
-                              w*stride_w:(w*stride_w)+patch_w] += preds[k]
+                    full_prob[i, :, h*self.stride_height:(h*self.stride_height)+patch_h,
+                              w*self.stride_width:(w*self.stride_width)+patch_w] += preds[k]
                     # Accumulate the number of predictions
-                    full_sum[i, :, h*stride_h:(h*stride_h)+patch_h,
-                             w*stride_w:(w*stride_w)+patch_w] += 1
+                    full_sum[i, :, h*self.stride_height:(h*self.stride_height)+patch_h,
+                             w*self.stride_width:(w*self.stride_width)+patch_w] += 1
                     k += 1
         assert (k == preds.shape[0])
         assert (np.min(full_sum) >= 1.0)
--- main.py
+++ main.py
@@ -1,33 +1,37 @@
-import torch 
+import torch
 from CNNs.inception_v3 import build_inception3_pretrained as build_model
 from dataloader import generate_test_data, generate_dataloader
 from train import train_process
 import torch.optim as optim
 from config import argparse
 from Vessel_Seg.export import vessel_seg_model
-args=argparse()
+args = argparse()
 print("Begin pretrain {}".format(args.pretrain))
-loss_func=torch.nn.CrossEntropyLoss()
+loss_func = torch.nn.CrossEntropyLoss()
 if args.GEN_DATA:
-        generate_test_data(PATH=args.PATH, TEST_DATA=args.TEST_DATA)
+    generate_test_data(PATH=args.PATH, TEST_DATA=args.TEST_DATA)
 
-train_loader, val_loader,test_loader, train_len, val_len,test_len ,num_class= generate_dataloader(PATH=args.PATH,
-                                                                     train_proportion=args.train_proportion, 
-                                                                     val_proportion=args.val_proportion,
-                                                                     batch_size=args.batch_size)
-model=build_model(num_classes=num_class,pretrained=args.pretrain).cuda()#todo model_setting
-Vessel_Seg_model=vessel_seg_model()
+train_loader, val_loader, test_loader, train_len, val_len, test_len, num_class = generate_dataloader(PATH=args.PATH,
+                                                                                                     train_proportion=args.train_proportion,
+                                                                                                     val_proportion=args.val_proportion,
+                                                                                                     batch_size=args.batch_size)
+model = build_model(num_classes=num_class,
+                    pretrained=args.pretrain).cuda()  # todo model_setting
+Vessel_Seg_model = vessel_seg_model(patch_height=args.ves_patch_height,
+                                    patch_width=args.ves_patch_wideth,
+                                    stride_height=args.ves_stride_height,
+                                    stride_width=args.ves_stride_width)
 
-train_processer=train_process(epoch=args.epoch,loss_func=loss_func)
-optimizer=optim.Adam(model.parameters(), lr=args.lr)
+train_processer = train_process(epoch=args.epoch, loss_func=loss_func)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
 train_processer.train(model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        test_loader=test_loader,
-        train_len=train_len,
-        val_len=val_len,
-        test_len=test_len,
-        optimizer=optimizer,
-        logging=True)
-print("data_size: train:{}, val:{}, test:{}".format(train_len,val_len,test_len))
+                      train_loader=train_loader,
+                      val_loader=val_loader,
+                      test_loader=test_loader,
+                      train_len=train_len,
+                      val_len=val_len,
+                      test_len=test_len,
+                      optimizer=optimizer,
+                      logging=True)
+print("data_size: train:{}, val:{}, test:{}".format(train_len, val_len, test_len))
