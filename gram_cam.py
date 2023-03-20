f2e6a3b3513282b6f479e94329ba38b12ef7a603
guoyi <1900013009@pku.edu.cn>
2023-03-20 16:54:37 +0800

    style

--- gram_cam.py
+++ gram_cam.py
@@ -1,6 +1,7 @@
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 import cv2
+# in new version of code, this file is useless
 def generate_Gram_CAM(model,target_layer,input_tensor,img_numpy,save_name):
     cam = GradCAM(model=model, target_layers=target_layer, use_cuda=True)
     # grayscale_cam = cam(input_tensor=input_tensor, targets=targets)  
