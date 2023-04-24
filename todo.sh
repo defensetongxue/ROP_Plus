65e059e26822ba33519d87d36497c4b9e55ce04f
defense <1900013009@pku.edu.cn>
2023-04-24 16:12:59 +0800

    refresh shell

--- todo.sh
+++ todo.sh
@@ -1,6 +1,3 @@
-python -u generate_data.py --data_file original
-python -u train.py --data_file original
-python -u generate_data.py --data_file replace_channel
-python -u train.py --data_file replace_channel
-python -u generate_data.py --data_file vessel
-python -u train.py --data_file vessel
+python -u cleansing.py
+python -u train.py
+python -u test.py
